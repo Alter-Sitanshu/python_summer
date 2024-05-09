038734d95da3c0a0bd9ddcb1916d2341e783707e
AlterSitanshu <sitanshumohapatra5@gmail.com>
2024-05-09 05:32:35 +0530

    calculator

---
+++ calc.py
@@ -0,0 +1,34 @@
+def add(num1,num2):
+    return num1 + num2
+def subtract(num1,num2):
+    return num1-num2
+def multiply(num1,num2):
+    return num1*num2
+def divide(num1,num2):
+    return num1/num2
+
+
+# taking the input frmo the user about the numbers
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+while(True):
+    operations = {"+":add,"-":subtract,"*":multiply,"/":divide}
+    for symbols in operations:
+        print(symbols,end=" ")
+    symbol = input("Choose a operation : ")
+    answer = operations[symbol](num1,num2)
+    print(f"{num1} {symbol} {num2} = {answer}")
+
+    breakout = input("Do you want to continue ?(yes/no)").lower()
+    if(breakout != "no"):
+        loop = input(f"Do you want to continue with {answer} ?(yes/no)").lower()
+        if(loop=="yes"):
+            num1 = answer
+            num2 = int(input("Enter the second number : "))
+        else:
+            num1 = int(input("Enter the first number : "))
+            num2 = int(input("Enter the second number : "))
+    else:
+        break
+    
