b4339adffc870dacfba9013269ca885450b04f0e
AlterSitanshu <sitanshumohapatra5@gmail.com>
2024-05-13 00:45:17 +0530

    library management

---
+++ library/librarydoc.py
@@ -0,0 +1,48 @@
+lib_books = {"roman invasion":{"name":"roman invasion", "author": "paul strauss"}}
+lib_users = {"sitanshu":"1234", "muskan":"hello", "steel":"iron"}
+
+class Library:
+    def __init__(self, user, password) :
+        self.user = user
+        self.password = password
+        self.books = {}
+    def login(self):
+        if self.user in lib_users:
+            if lib_users[self.user] == self.password:
+                print(f"Welcome to the library !")
+            else:
+                print(f"Wrong password for {self.user}")
+                return 0
+        else:
+            register = input("You are not a member, want to Register ?(yes/no) : ").lower()
+            if register == "yes":
+                lib_users[self.user] = self.password
+                print("New memeber added successfully !")
+            else:
+                return 0
+    
+    def issue_book(self, book_name):
+        self.books[book_name] = lib_books[book_name]
+        delete_book(book_name)
+    
+    def return_book(self, book_name):
+        book = self.books[book_name]
+        del self.books[book_name]
+        add_book(book["name"], book["author"])
+
+def add_book(book_name, author):
+        if book_name not in lib_books:
+            lib_books[book_name] = {"name":book_name, "author":author}
+            print(f"Thank you for submission, the book '{book_name}' has been added !")
+        else:
+            print("The book already exists, Thank you !")
+    
+def delete_book(book_name):
+    if book_name in lib_books:
+        del lib_books[book_name]
+    else:
+        print(f"The book '{book_name}' is not available in the library !")
+def view_books():
+    print("We have the following books :")
+    for books in lib_books:
+        print(books)
---
+++ library/main_lib.py
@@ -0,0 +1,49 @@
+import librarydoc
+
+enter = input("Login to the library ?(yes/no) : ").lower()
+if enter == "yes":
+    enter = True
+else:
+    enter = False
+action_func = {"add": librarydoc.add_book,
+               "delete": librarydoc.delete_book,
+               "view": librarydoc.view_books,
+               }
+while enter:
+    user = input("Enter your username : ").lower()
+    pasw = input("Enter your password : ")
+    login = librarydoc.Library(user=user,password=pasw)
+    check = login.login()
+    if check == 0:
+        print("Logging out from Library ...")
+        break
+    specific_functions = {"issue": login.issue_book, "return" : login.return_book}
+    print("General actions avaialable are : add book, delete book, view book\n")
+    action = input("What do you want to do ? : ")
+    if action == "add":
+        book_name = input("Enter book name : ").lower()
+        author = input("Enter book author : ").lower()
+        action_func[action](book_name=book_name,author=author)
+    elif action == "delete":
+        book_name = input("Enter book name : ").lower()
+        action_func[action](book_name=book_name)
+    elif action == "view":
+        action_func[action]()
+    else:
+        print("User specific actions are : issue book, return book")
+        specific_func = input("What do you want to do ? : ")
+        if specific_func == "issue":
+            book_name = input("Enter book name : ").lower()
+            specific_functions[specific_func](book_name=book_name)
+        elif specific_func == "return":
+            book_name = input("Enter book name : ").lower()
+            specific_functions[specific_func](book_name=book_name)
+        else:
+            print("Desired action is not avaialable currently ! Sorry")
+            enter = False
+            break
+    enter = input("Do you want to login again ?(yes/no) : ").lower()
+    if enter == "yes":
+        enter = True
+    else:
+        break
