0da6e845ad0c462ac9d26fb1c8b609771d7c4eb1
AlterSitanshu <sitanshumohapatra5@gmail.com>
2024-05-30 18:52:30 +0530

    pass manager with json

--- pass_manager/main.py
+++ pass_manager/main.py
@@ -1,14 +1,30 @@
 import tkinter as tk
 from tkinter import messagebox
+import json
+from json.decoder import JSONDecodeError
 FONT = ("Courier", 14, "bold")
 
 #----------Appending to the data file---------
 def save(website, email, password):
-    with open("data.txt",'a') as file:
-        file.write(f"{website} | {email} | {password}\n")
+    data_update = {website:{"email":email, "password":password}}
+    try:
+        with open("data.json", "r") as filepath:
+            data = json.load(filepath)
+            data.update(data_update)
+    except FileNotFoundError:
+        with open("data.json", "w") as file:
+            json.dump(data_update, file, indent=4)
+    except JSONDecodeError:
+        with open("data.json", "w") as file:
+            json.dump(data_update, file, indent=4)
+    else:
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent=4)
+    finally:
         web_text.delete(0, tk.END)
         gen_pass_text.delete(0, tk.END)
-        messagebox.showinfo(title="Succes", message="Saved Successfully")
+        messagebox.showinfo("success", "Added Successfully")
+
 def confirmation():
     website = web_text.get()
     email = email_text.get()
@@ -22,7 +38,21 @@ def confirmation():
         if is_ok:
             save(website, email, password)
 
-
+#---------Searching a certain website details-------
+def search():
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+            website = web_text.get()
+            info = data[website]
+    except FileNotFoundError:
+        messagebox.showerror("Error", "Not Found")
+    except KeyError:
+        messagebox.showerror("Error", "Not Found")
+    except JSONDecodeError:
+        messagebox.showerror("Error", "Not Found")
+    else:
+        messagebox.showinfo("Search Result", f"email:{info["email"]}\npassword:{info["password"]}")
 #---------Random password generator----------
 def random_pass():
     import random
@@ -60,17 +90,19 @@ web_label.grid(column=0, row=1, pady=5)
 email_label.grid(column=0, row=2, pady=5)
 gen_pass_label.grid(column=0, row=3, pady=5)
 
-web_text = tk.Entry(master=screen, font=FONT, width=35)
+web_text = tk.Entry(master=screen, font=FONT, width=25)
 web_text.focus()
 email_text = tk.Entry(master=screen, font=FONT, width=35)
 email_text.insert(0, string="sitanshu5@gmail.com")
 gen_pass_text = tk.Entry(master=screen, font= FONT, width=24)
-web_text.grid(column=1, row=1, columnspan=2, pady=5)
+web_text.grid(column=1, row=1, pady=5)
 email_text.grid(column=1, row=2, columnspan=2, pady=5)
 gen_pass_text.grid(column=1, row=3, pady=5)
 
 generate = tk.Button(text="Generate Password", pady=3, command=random_pass)
 add = tk.Button(text="Add", width=54, command=confirmation)
+search_button = tk.Button(text="Search", width=15, pady=3, command=search)
+search_button.grid(column=2, row=1)
 generate.grid(column=2, row=3, padx=5)
 add.grid(column=1, row=4, columnspan=2, pady=5)
 
