fb8dbcad4649d24a45c8910bd72d2a26bae21dcd
AlterSitanshu <sitanshumohapatra5@gmail.com>
2024-06-16 03:11:55 +0530

    simple portfolio using flask

---
+++ portfolio/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import smtplib
+import os
+
+RECEIVER = os.environ.get('RECEIVER')
+SENDER = os.environ.get('SENDER')
+PASS = os.environ.get("APP_PASS")
+
+app = Flask(__name__)
+connection = smtplib.SMTP('smtp.gmail.com')
+connection.starttls()
+connection.login(user=SENDER, password=PASS)
+
+@app.route('/')
+def home_render():
+    return render_template('index.html')
+
+@app.route('/submit', methods=["POST"])
+def handle_submit():
+    mail_address = request.form.get('Email')
+    message = request.form.get('message')
+    connection.sendmail(from_addr=SENDER, to_addrs=RECEIVER, msg=f"Subject: New Connewction\n\n{mail_address}\n\n{message}" )
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+        app.run(debug=True)
